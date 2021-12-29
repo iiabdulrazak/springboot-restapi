d80933ee38ec666f581cbff3e190edcc548a54d5
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-12-29 16:31:22 +0300

    Create DemoMicroRestapiApplication.java

---
+++ demo-micro-restapi/src/main/java/com/pinocchiovirus/demomicrorestapi/DemoMicroRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.pinocchiovirus.demomicrorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMicroRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMicroRestapiApplication.class, args);
+	}
+
+}
