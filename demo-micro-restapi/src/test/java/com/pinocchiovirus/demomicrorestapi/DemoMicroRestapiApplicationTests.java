9d83260ed051b4dc74b1b9dab60e935e00138e84
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-12-29 16:31:30 +0300

    Create DemoMicroRestapiApplicationTests.java

---
+++ demo-micro-restapi/src/test/java/com/pinocchiovirus/demomicrorestapi/DemoMicroRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinocchiovirus.demomicrorestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMicroRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
