7f72e8b08b16df1725838fc7cbf8b55f5722348e
Abdulrazak A. Osman <zaak0101@hotmail.com>
2021-12-30 13:10:40 +0300

    separating the business logic

--- demo-micro-restapi/src/main/java/com/pinocchiovirus/demomicrorestapi/coffee/CoffeeController.java
+++ demo-micro-restapi/src/main/java/com/pinocchiovirus/demomicrorestapi/coffee/CoffeeController.java
@@ -3,23 +3,19 @@ package com.pinocchiovirus.demomicrorestapi.coffee;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/coffee")
 public class CoffeeController {
+    private final CoffeeService coffeeService;
+
+    public CoffeeController(CoffeeService coffeeService) {
+        this.coffeeService = coffeeService;
+    }
     @GetMapping
     public List<Cofee> getCoffees(){
-        return List.of(
-                new Cofee(
-                        1L,
-                        "Abdulrazak A. Osman",
-                        LocalDate.of(2021, Month.DECEMBER, 29),
-                        "0547079680"
-                )
-        );
+        return coffeeService.getCoffees();
     }
+
 }
